bbb6cefd5a2080569c2da5e8f1aa84b55532440d
Aakar Singh <141477139+aakarrr@users.noreply.github.com>
2026-08-17 10:39:33 +0530

    Time: 4 ms (96.60%) | Memory: 44.6 MB (94.08%) - LeetSync

---
+++ LeetCode/71-simplify-path/simplify-path.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String token[]=path.split("/");
+        int n=token.length;
+        
+        StringBuilder sb=new StringBuilder();
+        int skip=0;
+        for(int i=n-1;i>=0;i--){
+            if(token[i].trim().equals(""))continue;
+            if(token[i].equals("..")){
+                skip++;
+                continue;
+            }
+            if(token[i].equals("."))continue;
+
+            if(skip>0){
+                skip--;
+                continue;
+            }
+
+            sb.insert(0,"/"+token[i]);
+        }
+        return sb.length()==0?"/":sb.toString();
+    }
+}
